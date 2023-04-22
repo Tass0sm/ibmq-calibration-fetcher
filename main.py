05cba6b8918b786fe3198c4414d51bc3e7870378
Tassos Manganaris <tassos.manganaris@gmail.com>
2023-04-22 16:43:23 -0400

    Last functional changes in main

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from fetcher import fetch_and_save_system_calibration_data
-from datetime import date
+from datetime import datetime
 
 driver = webdriver.Chrome()
-today = date.today()
+
+now = datetime.now().isoformat()
 
 for system in ["ibm_washington", "ibm_sherbrooke", "ibm_kyiv"]:
-    fetch_and_save_system_calibration_data(driver, system, today)
+    fetch_and_save_system_calibration_data(driver, system, now)
 
 driver.quit()
